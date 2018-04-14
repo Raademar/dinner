4dcfad8afcabe97b3bd016ecee0a1215c24666c0
Mattias <mattiasrademar@gmail.com>
2018-04-14 23:11:31 +0200

    continued working with the create happening page. working now, only missing users attending, proper image uploading and dynamic author.

--- admin/happening.php
+++ admin/happening.php
@@ -3,22 +3,43 @@
   include 'includes/head.php';
   include 'includes/navigation.php';
   //get happenings from database
-  $sql = "SELECT * FROM happening "; //WHERE date >= CURRENT_DATE ORDER BY id
+  $sql = "SELECT * FROM happening  WHERE date >= CURRENT_DATE ORDER BY date";
   $results = $db->query($sql);
   $errors = array();
+
+
   //If add form is submitted
   if(isset($_POST['add_submit'])){
-    $happening = $_POST['happening'];
+    $addhappening = sanitize($_POST['addhappening']);
+    $happeningtext = $_POST['happeningtext'];
+    $happeningdate = $_POST['happeningdate'];
+    $happeninguserid = $_POST['userid'];
+    $happeningimage = $_POST['imageuploader'];
+
+    $previewtext = substr($happeningtext, 0, 30).'...';
+
     //check if happening is blank
-    if($_POST['happening'] == ''){
-      $errors[].= 'You must input a Happening.';
+    if($_POST['addhappening'] == ''){
+      $errors[] .= 'You must input a Title.';
+    }
+    if($_POST['happeningtext'] == ''){
+      $errors[] .= 'You must input a description.';
+    }
+    if($_POST['happeningdate'] == ''){
+      $errors[] .= 'You must choose a date.';
+    }
+    if($_POST['userid'] == ''){
+      $errors[] .= 'You must insert your user ID.';
+    }
+    if($_POST['imageuploader'] == ''){
+      $errors[] .= 'You must upload an image.';
     }
     // check if happening exists in database
-    $sql = "SELECT * FROM happening WHERE title ='$happening'";
+    $sql = "SELECT * FROM happening WHERE title ='$addhappening'";
     $result = $db->query($sql);
     $count = mysqli_num_rows($result);
     if($count > 0){
-      $errors[] .= $happening.' already exsist. Please choose another name';
+      $errors[] = $addhappening.' already exsist. Please choose another name';
     }
 
     // display errors
@@ -26,8 +47,9 @@
       echo display_errors($errors);
     }else {
       //Add happening to database
-      $sql2 = "INSERT INTO happening (title) VALUES ('$happening')";
-      $db->query($sql2);
+      $sql = "INSERT INTO happening (title, preview_text, text, date, author, users_attending, image)
+      VALUES ('$addhappening', '$previewtext', '$happeningtext', '$happeningdate', '$happeninguserid', 'Some People', 'images/$happeningimage')";
+      $db->query($sql);
       header('Location: happening.php');
     }
 
@@ -36,31 +58,70 @@
   ?>
   <h2 class="text-center">Upcoming Happenings</h2><hr>
   <!-- Happenings form -->
-  <div class="d-flex justify-content-center">
-    <form class="form-inline" action="happening.php" method="post">
-      <div class="form-group">
-        <label for="happening">Add a Happening:</label>
-        <input type="text" name="happening" id="happening" class="form-control" value="<?=((isset($_POST['happening']))?$_POST['happening']:''); ?>">
+  <div class="row">
+    <div class="col-md-8">
+      <table class="table table-bordered table-striped table-auto table-sm">
+        <thead>
+          <th>Upcoming Happenings</th><th>Date</th>
+        </thead>
+        <tbody>
+          <?php while($happening = mysqli_fetch_assoc($results)): ?>
+            <tr>
+              <td><?=$happening['title'];?></td>
+              <td><?=$happening['date'];?></td>
+              <td>
+                <a href="happening.php?edit=<?=$happening['id'];?>" class="btn btn-sm btn-light"><span class="far fa-edit"></span></a>
+                <a href="happening.php?delete=<?=$happening['id'];?>" class="btn btn-sm btn-light"><span class="far fa-trash-alt"></span></a>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-md-4">
+      <form action="happening.php" method="post">
+        <div class="form-group">
+          <label for="happening">Title</label>
+          <input type="text" name="addhappening" id="addhappening" class="form-control" value="<?=((isset($_POST['addhappening']))?$_POST['addhappening']:''); ?>">
+        </div>
+          <div class="form-group">
+            <label for="happeningText">Say something about your Happening..</label>
+            <textarea class="form-control" name="happeningtext" id="happeningtext" type="text" rows="3" value="<?=((isset($_POST['happeningtext']))?$_POST['happeningtext']:''); ?>"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="pickFriends">Invite your friends..</label>
+            <select class="custom-select" multiple id="pickFriends" size="3">
+              <option value="1">Karl Klarksson</option>
+              <option value="2">Jenni Ferguson</option>
+              <option value="3">Stewie Wonder</option>
+              <option value="4">Mustafa Karim</option>
+              <option value="5">Malin Svensson</option>
+              <option value="6">Hanna Blue</option>
+              <option value="7">Derrick Johnsson</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="happeningdate">Pick a date for your Happening..</label><br>
+            <input id="happeningdate" name="happeningdate" type="date">
+          </div>
+          <div class="form-group">
+            <label for="imageuploader">Pick a picture for your Happening..</label>
+            <input type="file" class="form-control-file" name="imageuploader" id="imageuploader">
+          </div>
+          <div class="form-group">
+            <label for="userid">User ID..</label><br>
+            <input class="form-control" type="text" name="userid" id="userid" value="<?=((isset($_POST['userid']))?$_POST['userid']:''); ?>">
+          </div>
+          <!-- <div class="form-check">
+            <input class="form-check-input" type="radio" name="publicHappening" id="publicHappening1" value="public" checked>
+            <label class="form-check-label" for="publicHappening1">Public</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="privateHappening" id="privateHappening1" value="private">
+            <label class="form-check-label" for="privateHappening1">Private</label>
+          </div>  -->
         <input type="submit" name="add_submit" value="Add Happening" class="btn btn-outline-success">
-      </div>
-    </form>
+      </form>
+    </div>
   </div><hr>
-
-  <table class="table table-bordered table-striped table-auto">
-    <thead>
-      <th>Upcoming Happenings</th><th>Date</th>
-    </thead>
-    <tbody>
-      <?php while($happening = mysqli_fetch_assoc($results)): ?>
-        <tr>
-          <td><?=$happening['title'];?></td>
-          <td><?=$happening['date'];?></td>
-          <td>
-            <a href="happening.php?edit=<?=$happening['id'];?>" class="btn btn-sm btn-light"><span class="far fa-edit"></span></a>
-            <a href="happening.php?delete=<?=$happening['id'];?>" class="btn btn-sm btn-light"><span class="far fa-trash-alt"></span></a>
-          </td>
-        </tr>
-      <?php endwhile; ?>
-    </tbody>
-  </table>
 <?php  include 'includes/footer.php'; ?>
